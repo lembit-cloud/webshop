745d67e2c6d3346013b608f15f91203da5e73c42
Lembit <lembit.luik@outlook.com>
2021-02-11 20:01:10 +0200

    items-list,router-linkfavicon, stiilid

--- webshop-frontend/src/app/app-routing.module.ts
+++ webshop-frontend/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { ItemAddComponent } from './item/item-add/item-add.component';
 import { ItemEditComponent } from './item/item-edit/item-edit.component';
 import { ItemListComponent } from './item/item-list/item-list.component';
 import { ItemViewComponent } from './item/item-view/item-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-
+//only routes
+{ path: "", redirectTo: "home", pathMatch: "full"},
 { path: "home", component: ItemListComponent},
 { path: "item/edit", component: ItemEditComponent},
 { path: "item/view", component: ItemViewComponent},
 { path: "item/add", component: ItemAddComponent},
+{ path: "cart", component: CartComponent},
+//kõik ülejäänud roted
+{ path: "**", component: NotFoundComponent},
 
 ];
 
